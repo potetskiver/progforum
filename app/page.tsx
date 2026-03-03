1589a11727f9b032400ea62170938dd1b0ae9afe
Alexander Andersen <alexander.andersen@icloud.com>
2026-03-03 10:57:30 +0100

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> cb293e9 (Hello)
 // app/page.tsx
 export default function Home() {
   return (
@@ -11,27 +6,4 @@ export default function Home() {
       <p className="mt-4 text-lg text-gray-600">Your go-to place for programming discussions and resources.</p>
     </main>
   );
-<<<<<<< HEAD
 }
-=======
-import Image from "next/image";
-
-=======
-// app/page.tsx
->>>>>>> cb293e9 (Hello)
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to ProgForum!</h1>
-      <p className="mt-4 text-lg text-gray-600">Your go-to place for programming discussions and resources.</p>
-    </main>
-  );
-<<<<<<< HEAD
-}
->>>>>>> bb0efd9 (Initial commit from Create Next App)
-=======
-}
->>>>>>> cb293e9 (Hello)
-=======
-}
->>>>>>> cb293e9 (Hello)
