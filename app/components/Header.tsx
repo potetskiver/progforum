941e0c507e5becd5a32bf87e4a3af8eb48187128
Alexander Andersen <alexander.andersen@icloud.com>
2026-03-23 14:27:03 +0100

    Madikken!

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,30 @@
+"use client"
+
 import Link from "next/link";
 import { getUser } from "../lib/user";
 import LogoutButton from "./LogoutButton";
+import { useRouter } from "next/navigation";
 
 export default async function Header() {
   const user = await getUser();
 
+  const router = useRouter();
+
+  const changePassword = () => {
+    router.push("/changepassword");
+  }
+
   return (
-    <header className="bg-gray-800 text-white pl-4 pr-4 flex items-center justify-between">
+    <header className="bg-gray-800 text-white pl-4 pr-4 pt-1 flex items-center justify-between">
       <Link href="/" className="text-1xl font-bold">
         ProgForum
       </Link>
 
       {user ? (
         <div className="flex items-center">
-          <span className="mr-4 text-1xl">Hello, {user.name}!</span>
+          <span className="mr-4 text-1xl" onClick={changePassword}>
+            Hello, {user.name}!
+          </span>
           <LogoutButton />
         </div>
       ) : (
