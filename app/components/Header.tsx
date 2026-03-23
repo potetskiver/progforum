e862eb892e8c24156f8d122e18a87f788c70aac0
Alexander Andersen <alexander.andersen@icloud.com>
2026-03-23 14:39:20 +0100

    Hidden Admin button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,10 +5,6 @@ import LogoutButton from "./LogoutButton";
 export default async function Header() {
   const user = await getUser();
 
-  const changePassword = () => {
-    window.location.href = "/changepassword";
-  }
-
   return (
     <header className="bg-gray-800 text-white pl-4 pr-4 pt-1 flex items-center justify-between">
       <Link href="/" className="text-1xl font-bold">
@@ -17,8 +13,13 @@ export default async function Header() {
 
       {user ? (
         <div className="flex items-center mt-2">
+          {user.admin && (
+            <Link href="/admin" className="underline text-red-400">
+              Admin
+            </Link>
+          )}
           <Link href="/changepassword" className="text-1xl">
-            <span className="mr-2 text-1xl">
+            <span className="mr-2 ml-2 text-1xl">
               Hello, {user.name}!
             </span>
           </Link>
